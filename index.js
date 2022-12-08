6d814912708135f1f35c467173d1668b0fde6d69
MaverickEdwards <r.edwards0917@gmail.com>
2022-12-07 22:30:19 -0500

    commit tree 71044ceda3fa6b418862735c2778224d84ecb5be

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your code here
+function submitData(name, email){
+    const configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    };
+    return fetch("http://localhost:3000/users",configObj)
+    .then(resp => resp.json())
+    .then(body => addNewContact(body.id))
+    .catch(message => addNewContact(message.message))
+}
+function addNewContact(newID){
+    document.body.innerHTML = newID
+}
